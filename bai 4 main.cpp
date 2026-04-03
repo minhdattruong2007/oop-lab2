86c1663fd3f1157d49a722109b6e63c748c274fd
minhdattruong2007 <minhdattruong2007@gmail.com>
2026-04-03 21:43:28 +0700

    Create bai 4 main.cpp

---
+++ bai 4 main.cpp
@@ -0,0 +1,14 @@
+#include "GioPhutGiay.h"
+
+int main(){
+    GioPhutGiay t;
+
+    t.Nhap();
+
+    t.TinhCongThemMotGiay(); // cộng thêm 1 giây
+
+    t.Xuat(); // chỉ in kết quả
+
+    return 0;
+}
+
